8e0eef789c2b94328bb23953fcec471e929b404e
yukimir <luodadi.huazhu@gmail.com>
2018-10-24 18:38:52 +0900

    feat(assets.controller): rest

--- src/assets/assets.controller.ts
+++ src/assets/assets.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Res } from '@nestjs/common';
+import { Controller, Get, Post, Body, Res, Query } from '@nestjs/common';
 import { FileRequest, RequestFileInfo, SubmitWork, ContractProposal, Section } from './interface/service.interface';
 import { CmUpdateDto } from './dto/communication.dto';
 import { AssetsService } from './assets.service';
@@ -8,25 +8,25 @@ export class AssetsController {
         private readonly assetsService: AssetsService) { }
     @Post('update') // 更新数据库文件
     async updateFiles(@Body() updateCommand: CmUpdateDto): Promise<string> {
-        return this.assetsService.updateWeekly(updateCommand);
+        return await this.assetsService.updateWeekly(updateCommand);
     }
-    @Get('request_files_info') // 请求文件列表
+    @Get('files') // 请求文件列表
     async getFilesInfo(): Promise<RequestFileInfo[]> {
-        return this.assetsService.getFilesInfo();
+        return await this.assetsService.getFilesInfo();
     }
 
-    @Post('request_file') // 请求单个文件内容
-    async getFile(@Body() fileRequest: FileRequest): Promise<Array<Section>> {
-        return this.assetsService.getFile(fileRequest);
+    @Get('file') // 请求单个文件内容
+    async getFile(@Query() fileRequest: FileRequest): Promise<Array<Section>> {
+        return await this.assetsService.getFile(fileRequest);
     }
 
-    @Post('submit_work') // 提交工作
+    @Post('work') // 提交工作
     async submitWork(@Body() submitedWork: SubmitWork): Promise<string> {
-        return this.assetsService.submitWork(submitedWork);
+        return await this.assetsService.submitWork(submitedWork);
     }
 
     @Post('contract') // 圈文运动
     async contract(@Body() proposal: ContractProposal): Promise<string> {
-        return this.assetsService.contract(proposal);
+        return await this.assetsService.contract(proposal);
     }
 }
